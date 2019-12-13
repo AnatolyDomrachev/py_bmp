c7448e536b0102f97f57b2b1a0559608be12deb4
Your Name <you@example.com>
2019-12-13 13:34:52 +0300

    2

--- main.py
+++ main.py
@@ -23,3 +23,14 @@ print(head_size)
 buf = f.read(4)
 width = struct.unpack('i', buf)
 print(width)
+
+buf = f.read(4)
+height = struct.unpack('i', buf)
+print(height)
+
+buf = f.read(4*7)
+
+buf = f.read(3)
+pixel = struct.unpack('BBB', buf)
+print(pixel)
+
